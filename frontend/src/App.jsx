1241960f95af343ffc235619e8df4653abfb1cab
Devansh Garg <devanshgargmg@gmail.com>
2026-03-02 12:53:36 +0530

    fix loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,10 +14,7 @@ const App = () => {
   return (
     <div className="relative h-full w-full">
       <Toaster position="top-center" />
-      <div
-        className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 
-        [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]"
-      />
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white" />
 
       <Routes>
         {/* Public routes */}
